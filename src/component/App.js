c155f2732e65de5c95d3722ccd6b2aec29455a37
Itsuki <t.itsuki928@gmail.com>
2021-02-19 03:00:56 +0900

    🩹 Change the document title
    public folder内の<title>を変更したが、変わらなかったため、Phone component内でuseEffect()を使いタイトルを変えたところ上手くいった。

--- src/component/App.js
+++ src/component/App.js
@@ -1,7 +1,11 @@
+import { useEffect } from 'react';
 import Phone from './Phone';
 import '../css/app.css';
 
 function App() {
+	useEffect(() => {
+		document.title = 'Tinder UI';
+	}, []);
 	return <Phone />;
 }
 export default App;
