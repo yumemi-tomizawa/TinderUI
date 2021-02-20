9eb69f4eaf41647e1f58eb640c45776a565c485a
Itsuki <t.itsuki928@gmail.com>
2021-02-20 14:01:24 +0900

    ✨ Retrive images using Pixabay API
    コンポーネントが最初にマウントされた瞬間にfetch APIを使い外部から女の子たちの画像を取得。
    画像のAPIは無料のPixabay APIを使用。
    しかし、画像のクオリティが著しくよくないためUnsplash APIの使用を検討。

--- src/component/Phone.js
+++ src/component/Phone.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TinderCards from './TinderCards';
 import Button from './Button';
 
@@ -6,32 +6,25 @@ export default function Phone() {
 	// This keeps track of swipe direction.
 	const [swipeDirection, setSwipeDirection] = useState('');
 	// This data will be passed to <TinderCards> Component.
-	const [people, setPeople] = useState([
-		{
-			nickname: 'Sara',
-			age: 18,
-			url:
-				'https://images.unsplash.com/photo-1594744803329-e58b31de8bf5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-		},
-		{
-			nickname: 'Yui',
-			age: 29,
-			url:
-				'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-		},
-		{
-			nickname: 'Vennesa',
-			age: 20,
-			url:
-				'https://images.unsplash.com/photo-1504730030853-eff311f57d3c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-		},
-		{
-			nickname: 'Karen',
-			age: 24,
-			url:
-				'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-		},
-	]);
+	const [people, setPeople] = useState([]);
+
+	useEffect(() => {
+		// This is Pixabay API
+		const api = '19800860-fd81fde52b5686725fdcf6309',
+			keyword = 'model+face',
+			num_photo = 4;
+		fetch(
+			`https://pixabay.com/api/?key=${api}&q=${keyword}&image_type=photo&page=9&per_page=${num_photo}`
+		)
+			.then((pictures) => pictures.json())
+			.then((pictures) => {
+				pictures.hits.forEach((picture, index) => {
+					const newPerson = { ...profiles[index], url: picture.previewURL };
+					profiles[index] = newPerson;
+				});
+				setPeople(profiles);
+			});
+	}, []);
 
 	function handleDelete(direction) {
 		// If direction is not same, update swipe direction and rerender DOM then className of CSSTransition in TinderCards.js will be updated so that card will be swiped based on user input.
@@ -49,9 +42,36 @@ export default function Phone() {
 	}
 
 	return (
-		<div className='phone'>
-			<TinderCards swipeDirection={swipeDirection} people={people} />
-			<Button handleDelete={handleDelete} />
-		</div>
+		<>
+			{people.length !== 0 && (
+				<div className='phone'>
+					<TinderCards swipeDirection={swipeDirection} people={people} />
+					<Button handleDelete={handleDelete} />
+				</div>
+			)}
+		</>
 	);
 }
+
+const profiles = [
+	{
+		nickname: 'Sara',
+		age: 18,
+		url: '',
+	},
+	{
+		nickname: 'Yui',
+		age: 29,
+		url: '',
+	},
+	{
+		nickname: 'Vennesa',
+		age: 20,
+		url: '',
+	},
+	{
+		nickname: 'Karen',
+		age: 24,
+		url: '',
+	},
+];
