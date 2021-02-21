27b3dac59a5d86a346e1f55b6833954c2a66566c
Itsuki <t.itsuki928@gmail.com>
2021-02-21 19:16:40 +0900

    Updates

---
+++ precache-manifest.9db6776f356ff4d813173520842fb984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f9c5294190cfc95c0e3",
+    "url": "/TinderUI/static/css/main.1ed2687c.chunk.css"
+  },
+  {
+    "revision": "1f9c5294190cfc95c0e3",
+    "url": "/TinderUI/static/js/main.1f9c5294.chunk.js"
+  },
+  {
+    "revision": "7cd49296196840daea4e",
+    "url": "/TinderUI/static/js/1.7cd49296.chunk.js"
+  },
+  {
+    "revision": "1fd7459334d33e78a221",
+    "url": "/TinderUI/static/js/runtime~main.1fd74593.js"
+  },
+  {
+    "revision": "bb60a97e23659b7496205ad0b9f835ce",
+    "url": "/TinderUI/index.html"
+  }
+];
